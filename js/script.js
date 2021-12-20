22b8ad129f6942f166655d175d0b79772064bd1d
niculas1989 <nicolas.maranzano@libero.it>
2021-12-20 14:16:53 +0100

    added analyses for JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 /*
 Traccia:
 Chiedi all’utente il suo nome,
-    poi chiedi il suo cognome,
-        poi chiedi il suo colore preferito
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+Partiamo analizzando il "problema" che abbiamo davanti.
+
+Come primo step, partiamo facendo apparire sullo schermo un "prompt" in cui chiediamo il nome (in questo caso va bene come identificativo "firstName"), dopodicché andiamo avanti facendo apparire su schermo "secondName" per il cognome.
+Infine, chiediamo all'utente quale sia il suo colore preferito ("favouriteColor" può andare più che bene).
+Come ultimo step combiniamo i 3 identificativi per ottenere una password a cui aggiungiamo il valore "21" (da capire se definito prima da noi, o assegnare un valore come "currentYear")
 
 
 
