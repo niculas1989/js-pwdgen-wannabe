a4599487f6e3383758f6f9ac11ebabba345ecd76
niculas1989 <nicolas.maranzano@libero.it>
2021-12-20 14:09:16 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Traccia:
+Chiedi all’utente il suo nome,
+    poi chiedi il suo cognome,
+        poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+
+
+
+
+
+*/
